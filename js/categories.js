049f34351f17b829a405a6a131b0cdbbe7df5d4f
Jeremy <jeremy.cassou@gmail.com>
2018-10-19 14:53:33 +0200

    Ajout des catégories existantes

---
+++ js/categories.js
@@ -0,0 +1,130 @@
+var categories = [
+  {
+    "id": 742,
+    "name": "Hôtels"
+  },
+  {
+    "id": 749,
+    "name": "Retour location de voitures"
+  },
+  {
+    "id": 247,
+    "name": "Boutiques"
+  },
+  {
+    "id": 691,
+    "name": "Covoiturage"
+  },
+  {
+    "id": 248,
+    "name": "Cafés & restaurant"
+  },
+  {
+    "id": 274,
+    "name": "Services financiers"
+  },
+  {
+    "id": 252,
+    "name": "Salon \"la croix du Sud\""
+  },
+  {
+    "id": 620,
+    "name": "Fontaines à eau"
+  },
+  {
+    "id": 304,
+    "name": "Portes d'embarquement"
+  },
+  {
+    "id": 284,
+    "name": "Enregistrement"
+  },
+  {
+    "id": 245,
+    "name": "Parcs autos"
+  },
+  {
+    "id": 244,
+    "name": "Navette parcs autos"
+  },
+  {
+    "id": 272,
+    "name": "Parc motos"
+  },
+  {
+    "id": 677,
+    "name": "Parcs vélos"
+  },
+  {
+    "id": 246,
+    "name": "Transport en commun"
+  },
+  {
+    "id": 676,
+    "name": "Taxis"
+  },
+  {
+    "id": 250,
+    "name": "Location de voitures"
+  },
+  {
+    "id": 288,
+    "name": "Accueil information"
+  },
+  {
+    "id": 275,
+    "name": "Services pratiques"
+  },
+  {
+    "id": 276,
+    "name": "Services Famille"
+  },
+  {
+    "id": 736,
+    "name": "Espaces bébés"
+  },
+  {
+    "id": 277,
+    "name": "Poste premier secours & Douanes"
+  },
+  {
+    "id": 289,
+    "name": "Tours opérateurs"
+  },
+  {
+    "id": 273,
+    "name": "Distributeurs boissons & confiseries"
+  },
+  {
+    "id": 285,
+    "name": "Poste inspection filtrage"
+  },
+  {
+    "id": 283,
+    "name": "Entrées & sorties des halls vers l'exterieur"
+  },
+  {
+    "id": 278,
+    "name": "Bagages"
+  },
+  {
+    "id": 280,
+    "name": "Ascenceurs"
+  },
+  {
+    "id": 281,
+    "name": "Escaliers"
+  },
+  {
+    "id": 282,
+    "name": "Escalators"
+  },
+  {
+    "id": 279,
+    "name": "Comptoir compagnie aérienne /assistants"
+  },
+  {
+    "id": 253,
+    "name": "Toilettes"
+  }
+]
